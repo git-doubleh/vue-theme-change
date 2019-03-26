bfd1b7951811b1229669804847d7f6c3037e2ce4
git-doubleh <1874165806@qq.com>
2019-03-26 15:05:52 +0800

    主题切换

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="body">
+      <div class="div"></div>
+      <span class="text">颜色改变</span>
+    </div>
+    <select
+      name=""
+      id=""
+      @change="toggleBg($event)"
+    >
+      <option value="theme1">红</option>
+      <option value="theme2">黑</option>
+    </select>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
   components: {
-    HelloWorld
+  },
+  methods: {
+    toggleBg (e) {
+      window.document.documentElement.setAttribute('data-theme', e.target.value)
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+
+@import '../assets/mixin.scss';
+@import '../assets/variable.scss';
+.home {
+  width: 100%;
+  .body {
+    @include setbgColor('bg');
+    height: 100px;
+    .div{
+      @include setbgColor('bg-sec');
+      @include border($theme1-border);
+      width: 50px;
+      height: 50px;
+    }
+    .text{
+      @include setFontColor('fc-main');
+    }
+  }
+}
+</style>
+
